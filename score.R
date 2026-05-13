47443ba4b7d999419d79d4bb14f2eb25b7c0c96c
jamu3l <119085608+jamu3l@users.noreply.github.com>
2026-05-13 18:12:13 +0200

    ajout fonction walkscore

--- score.R
+++ score.R
@@ -1,11 +1,25 @@
 # Copyright (c) 2026 Samuel Polat
-# Sous licence CC-BY 4.0 : https://creativecommons.org/licenses/by/4.0/
-# Voir le fichier LICENSE pour les conditions complètes.
 
 library(readr)
 library(dplyr)
+library(httr2)
 
 # Importer jeu de données
 ime <- read_csv("ime-idf-geocod-tri.csv") %>%
   # Ne conserver que les lignes contenant des coordonnées valides
   filter(!is.na(latitude), !is.na(longitude))
+
+# Fonction pour obtenir le Walkscore d'un lieu spécifique
+get_walkscore <- function(lat, lon) {
+  res <- request("https://api.walkscore.com/score") %>%
+    req_url_query(
+      format = "json",
+      lat = lat,
+      lon = lon,
+      wsapikey = Sys.getenv("WALKSCORE_API_KEY")
+    ) %>%
+    req_perform() %>%
+    resp_body_json()
+  
+  res$walkscore
+}
