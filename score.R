3d5553c233a48692218fbd322cf56a6b7792433f
jamu3l <119085608+jamu3l@users.noreply.github.com>
2026-05-13 16:30:28 +0200

    importer données

---
+++ score.R
@@ -0,0 +1,9 @@
+# Copyright (c) 2026 Samuel Polat
+# Sous licence CC-BY 4.0 : https://creativecommons.org/licenses/by/4.0/
+# Voir le fichier LICENSE pour les conditions complètes.
+
+library(readr)
+library(dplyr)
+
+# Importer jeu de données
+ime <- read_csv("ime-idf-geocod-tri.csv")
